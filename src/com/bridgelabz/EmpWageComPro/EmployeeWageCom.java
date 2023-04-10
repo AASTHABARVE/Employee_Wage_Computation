eca28891a640f634a341a1df7ae0c82fdd5a4d62
aastha <aasthabarve0@gmail.com>
2023-04-10 22:11:43 +0530

    add: welcome message

---
+++ src/com/bridgelabz/EmpWageComPro/EmployeeWageCom.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.EmpWageComPro;
+
+public class EmployeeWageCom {
+    public static void main(String args[]){
+        System.out.println("Welcome to employee wage");
+    }
+
+}
